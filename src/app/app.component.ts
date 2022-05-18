bdd179c0c2f7a573fedbd5eb3e1a96d46a9098f9
Juan Torres <jstorresg@misena.edu.co>
2022-05-18 16:44:00 -0500

    se sube el front del formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import * as Notiflix from 'notiflix';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Formulario';
+  datos:FormGroup;
+
+  constructor(private httpclient:HttpClient){
+    this.datos = new FormGroup({
+      correo: new FormControl('',[Validators.required, Validators.email]),
+      asunto: new FormControl('',[Validators.required]),
+      mensaje: new FormControl('',[Validators.required])
+    });
+  }
+
+  enviocorreos(){
+    Notiflix.Loading.standard("Cargando... ")
+    let params = {
+      email:this.datos.value.correo,
+      asunto:this.datos.value.asunto,
+      mensaje:this.datos.value.mensaje
+    }
+
+    //console.log(params)
+    this.httpclient.post('http://localhost:3000/envio',params).subscribe(response => {
+      console.log(response)
+      Notiflix.Loading.remove();
+      Notiflix.Notify.success("Enviado correctamente.");
+    })
+  }
 }
+
